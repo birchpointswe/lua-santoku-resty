4a3fc86cf87adf09cbc2b516968f0122dbe7e75b
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-10 16:29:25 -0400

    bump version to 2.0.0-1

--- make.lua
+++ make.lua
@@ -1,11 +1,11 @@
 local env = {
   name = "santoku-resty",
-  version = "1.0.0-1",
+  version = "2.0.0-1",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
-    "santoku >= 1.0.0, < 2.0.0",
+    "santoku >= 2.0.0, < 3.0.0",
     "lua-resty-http == 0.17.2-0",
     "lua-resty-openssl == 1.5.1-1",
     "lua-resty-jwt == 0.2.3-0",
